c107774ee7dc521bc3d817e9a831ca92bd301302
Cesar Agusto Morales Xochipiltecatl <mcesar@gatech.edu>
2025-04-18 15:58:52 -0400

    fixes for ros pkg

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ package_name = 'flir_lepton_human_detector'
 setup(
     name=package_name,
     version='0.0.1',
-    packages=package_name(exclude=['test']),
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
